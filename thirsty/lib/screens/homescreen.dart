c32c7043f6b713e373c95fc745fe7a6de5bd0600
Hyunkdev <hyunkddev@gmail.com>
2024-11-17 19:13:57 +0900

    Home widget done

--- thirsty/lib/screens/homescreen.dart
+++ thirsty/lib/screens/homescreen.dart
@@ -2,95 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:thirsty/provider/bottom_nav_provider.dart';
 import 'package:thirsty/provider/countprovider.dart';
+import 'package:thirsty/widgets/mainscreenwidget.dart';
 
 class Homescreen extends StatelessWidget {
   Homescreen({super.key});
   late Countprovider _countprovider;
   late BottomNavProvider _bottomNavProvider;
 
+  Widget _navigationBody() {
+    switch (_bottomNavProvider.currentPage) {
+      case 0:
+        return Mainscreenwidget();
+
+      case 1:
+        break;
+    }
+    return Container();
+  }
+
   Widget _bottomNavigationBar() {
-    return Consumer<BottomNavProvider>(
-      builder: (context, provider, widget) {
-        return BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.collections_bookmark_rounded),
-                label: 'Collections'),
-          ],
-          currentIndex: _bottomNavProvider.currentPage,
-          selectedItemColor: Colors.blueAccent,
-          onTap: (index) {
-            _bottomNavProvider.updateCurrentPage(index);
-          },
-        );
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.collections_bookmark_rounded),
+            label: 'Collections'),
+      ],
+      currentIndex: _bottomNavProvider.currentPage,
+      selectedItemColor: Colors.blueAccent,
+      onTap: (index) {
+        _bottomNavProvider.updateCurrentPage(index);
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build called");
-    _countprovider = Provider.of<Countprovider>(context, listen: false);
-    _bottomNavProvider = Provider.of<BottomNavProvider>(context, listen: false);
+    _bottomNavProvider = Provider.of<BottomNavProvider>(context);
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 97, 155, 255),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Consumer<Countprovider>(
-                builder: (context, provider, child) {
-                  return Text(
-                    provider.check.toString() == 'true'
-                        ? '휴.. 살것같군..'
-                        : '목말라..',
-                    style: const TextStyle(
-                      fontSize: 40,
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Consumer<Countprovider>(
-                builder: (context, provider, child) {
-                  return InkWell(
-                    onTap: () {
-                      _countprovider.add();
-                    },
-                    //<a href="https://www.flaticon.com/kr/free-icons/-" title="물기 없는 아이콘">물기 없는 아이콘 제작자: amonrat rungreangfangsai - Flaticon</a>
-                    child: Image.asset(
-                      provider.check.toString() == 'false'
-                          ? 'assets/thirsty.png'
-                          : 'assets/no_thirsty.png',
-                      scale: 1.4,
-                    ),
-                  );
-                },
-              ),
-              Consumer<Countprovider>(
-                builder: (context, provider, child) {
-                  return Column(
-                    children: [
-                      Text(
-                        provider.count.toString(),
-                      ),
-                      Text('남은 횟수 : ${provider.grade.toString()} / 3'),
-                    ],
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: _navigationBody(),
       bottomNavigationBar: _bottomNavigationBar(),
     );
   }
---
+++ thirsty/lib/widgets/mainscreenwidget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:thirsty/provider/countprovider.dart';
+
+class Mainscreenwidget extends StatelessWidget {
+  Mainscreenwidget({super.key});
+
+  late Countprovider _countprovider;
+
+  @override
+  Widget build(BuildContext context) {
+    _countprovider = Provider.of<Countprovider>(context);
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 97, 155, 255),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Consumer<Countprovider>(
+                builder: (context, provider, child) {
+                  return Text(
+                    provider.check.toString() == 'true'
+                        ? '휴.. 살것같군..'
+                        : '목말라..',
+                    style: const TextStyle(
+                      fontSize: 40,
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Consumer<Countprovider>(
+                builder: (context, provider, child) {
+                  return InkWell(
+                    onTap: () {
+                      _countprovider.add();
+                    },
+                    //<a href="https://www.flaticon.com/kr/free-icons/-" title="물기 없는 아이콘">물기 없는 아이콘 제작자: amonrat rungreangfangsai - Flaticon</a>
+                    child: Image.asset(
+                      provider.check.toString() == 'false'
+                          ? 'assets/thirsty.png'
+                          : 'assets/no_thirsty.png',
+                      scale: 1.4,
+                    ),
+                  );
+                },
+              ),
+              Consumer<Countprovider>(
+                builder: (context, provider, child) {
+                  return Column(
+                    children: [
+                      Text(
+                        provider.count.toString(),
+                      ),
+                      Text('남은 횟수 : ${provider.grade.toString()} / 3'),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
